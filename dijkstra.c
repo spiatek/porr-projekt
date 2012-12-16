6330a2c90b05a989d9fc2cd4bc4b2e06bfe7637f
Szymon Piątek <spiatheck@gmail.com>
2012-12-16 03:13:43 +0100

    reimplementation of Dijkstra algorithm with sparse matrix representation

--- dijkstra.c
+++ dijkstra.c
@@ -1,85 +1,126 @@
 #include <stdio.h>
+#include <time.h>
 
-#define MAX 7
-#define INF 999
+#include "read_network.c"
 
-int auction_search(int *pr, int *P, int a[][MAX], int s, int t)
+#define INFINITE 9999999
+
+int allselected(int *selected, int n)
 {
-	int i, length = 1, j = t;
-	int la, maxla, argmaxla;
-	if(s == t) {
-		printf("s = t, brak sciezki\n");
-		return 0;
-	}
-	for(i = 0; i < MAX; i++) {
-		P[i] = INF;
-		pr[i] = 0;
-	}
-	if(t >= 0 && t < MAX) {
-		P[t] = 0;
-	}
-	else {
-		printf("Blad, t musi byc z zakresu 0..MAX-1\n");
-		return 0;
-	}
-	if(s < 0 || s >= MAX) {
-		printf("Blad, s musi byc z zakresu 0..MAX-1\n");
-		return 0;
-	}
-	while(P[s] == INF) {
-		printf("Sciezka: ");
-		for(i = 0; i < MAX; i++) {
-			printf("%d ", P[i]);
+	int i;
+	for(i = 1; i < n + 1; i++) {
+		if(selected[i] == 0) {
+			return 0;
 		}
-		printf("\n");
-		maxla = 0 - INF;
-		argmaxla = -1;
-		printf("j = %d\n", j);
-		for(i = 0; i < MAX; i++) {
-			la = pr[i] - a[i][j];
-			if(la > maxla && P[i] == INF) {	//pomijane sa te, ktore juz sa w P
-				maxla = la;
-				argmaxla = i;
+	}
+	return 1;
+}
+
+void shortpath(int (*cost)[2], int *preced, int *distance, int nodes, int arcs, int s, int t)
+{
+	int current = t, i, j, k, dc, smalldist, newdist;
+	int selected[nodes + 1];
+	
+	for(i = 0; i <= nodes; i++) {
+		distance[i] = INFINITE;
+		selected[i] = 0;
+	}
+	distance[current] = 0;
+	
+	
+	while(!allselected(selected, nodes))
+	{
+        	smalldist = INFINITE;
+		dc = distance[current];
+		k = 0;
+
+		//printf("%d ", current);
+		/* wyszukanie nieodwiedzonego wierzcholka (k) z najmniejszym distance (smalldist) */
+		for(i = 1; i <= nodes; i++) {
+			if(selected[i] == 0 && distance[i] < smalldist) {
+				//printf("nowe distance[j] < smalldist i rowne\n", distance[i]);
+				smalldist = distance[i];
+				k = i;
+				//printf("nowym wezlem bedzie %d\n", k);
 			}
 		}
-		if(pr[j] > maxla) {
-			printf("Contracting path\n");
-			pr[j] = maxla;
-			if(j != t) {
-				P[j] = INF;
-				length = length - 1;
-				for(i = 0; i < MAX; i++) {		//wraca do poprzedniego j
-					if(P[i] == length - 1) {
-						j = i;
-						break;
+		
+		if(k != 0) {
+			current = k;
+			//printf("nowa iteracja z %d\n", current);
+			selected[current] = 1;
+
+			/* dotarcie do sasiadow current */
+			for(i = 0; i < nodes + arcs; i++) {
+				if(cost[i][0] == 0 && cost[i][1] == current) {
+					break;
+				}
+			}
+	
+			while(i < nodes + arcs) {
+				i++;
+				j = cost[i][0];
+				if(j == 0) {
+					break;
+				}
+				//printf("rozp. wezel: %d\n", j);
+				if(selected[j] == 0) {
+					//printf("wezel %d jeszcze nie byl wybierany\n", j);
+					//printf("jego koszt: %d\n", cost[i][1]);
+					newdist = dc + cost[i][1];
+					//printf("newdist: %d\n", newdist);
+					if(newdist < distance[j]) {
+						//printf("newdist < distance[j]\n");
+						distance[j] = newdist;
+						preced[j] = current;
 					}
 				}
 			}
 		}
-		else {
-			printf("Extending path\n");
-			P[argmaxla] = length;
-			j = argmaxla;
-			length = length + 1;
-			if(argmaxla == s)
-				return 1;
-		}
 	}
-	return 0;
+	//for(i = 0; i <= nodes; i++) {
+       	//	printf("%d ", preced[i]);
+	//}
+	//printf("\n");
 }
 
 int main(int argc, char* argv[])
 {
-	int a[MAX][MAX]={{INF,2,4,7,INF,5,INF},{2,INF,INF,6,3,INF,8},{4,INF,INF,INF,INF,6,INF},{7,6,INF,INF,INF,1,6},{INF,3,INF,INF,INF,INF,7},{5,INF,6,1,INF,INF,6},{INF,8,INF,6,7,6,INF}};
-	int prices[MAX];
-	int P[MAX];
-	int i, source, tail;
-	source = atoi(argv[1]);
-	tail = atoi(argv[2]);
-	auction_search(prices, P, a, source, tail);
-	for(i = 0; i < MAX; i++) {
-		printf("%d ", P[i]);
+	double time;
+	int *preced, *distance;
+	int source, tail, nodes, arcs, i;
+	int (*network)[2];
+	clock_t start, end;
+	char *filename;
+
+	arcs = atoi(argv[1]);
+	nodes = atoi(argv[2]);
+	filename = argv[3];
+
+	network = (int (*)[2])malloc((arcs+nodes)*2*sizeof(int));		//przydzial pamieci dla tablicy z grafem
+	
+	read_network(filename, &source, &tail, &nodes, &arcs, network);
+	
+	preced = (int*)malloc((nodes+1)*sizeof(int));
+	for(i = 0; i < nodes; i++) {
+		preced[i] = 0;
 	}
-	printf("\n");
+	distance = (int*)malloc((nodes+1)*sizeof(int));
+	
+	start = clock();
+	shortpath(network, preced, distance, nodes, arcs, source, tail);	
+	end = clock();
+	
+	time = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+	printf("Czas wykonania programu: %5.1f [ms]\n", time*1000);
+	//for(i = 0; i < nodes; i++) {
+	//	printf("%d ", P[i]);
+	//}
+	//printf("\n");
+	//for(i = 0; i <= nodes; i++) {
+       	//	printf("%d\n", distance[i]);
+	//}
 	return 0;
 }
+
